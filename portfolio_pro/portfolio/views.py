d6df94b3af4de8e88534e3d1a2093e3dc960d2c7
anitapatil2814 <anitapatil2814@gmail.com>
2024-04-22 08:14:04 +0530

    first commit

--- portfolio_pro/portfolio/views.py
+++ portfolio_pro/portfolio/views.py
@@ -8,14 +8,15 @@ from django.contrib import messages
 def home(request):
      return render(request,"index.html")
 def contact(request):
-    if request.method=="POST":
-     contact=Contact()
-     name=request.POST.get("name")
-     email=request.POST.get("email")
-     message=request.POST.get("message") 
-     contact.name=name 
-     contact.email=email
-     contact.message=message
-     contact.save()
-     return HttpResponse("Thank For Contact Me !!")    
-    return render(request,'index.html')
+     if request.method=="POST":
+          contact=Contact()
+          name=request.POST.get("name")
+          email=request.POST.get("email")
+          message=request.POST.get("message") 
+          contact.name=name 
+          contact.email=email
+          contact.message=message
+          contact.save()
+          context={}
+          context['success']="Thank For Contact Me !!"
+     return render(request,'index.html',context)
